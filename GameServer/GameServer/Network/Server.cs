d47a361378f0a3a73744fd316b2a783fc08e212e
Luke <luke.thienemann@icloud.com>
2020-05-14 17:26:29 +0200

    test update

--- GameServer/GameServer/Network/Server.cs
+++ GameServer/GameServer/Network/Server.cs
@@ -29,7 +29,7 @@ namespace GameServer.Network
 
             InitializeServerData();
 
-            _tcpListener = new TcpListener(IPAddress.Parse(Constants.RUNNING_IP), Constants.RUNNING_PORT);
+            _tcpListener = new TcpListener(IPAddress.Parse("127.0.0.1"), Constants.RUNNING_PORT);
             _tcpListener.Start();
             _tcpListener.BeginAcceptTcpClient(new AsyncCallback(TcpConnectCallback), null);
 
